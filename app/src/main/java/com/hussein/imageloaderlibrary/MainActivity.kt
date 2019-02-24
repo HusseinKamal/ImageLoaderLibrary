d91adfe4808f3240c325da48d2df6d517a20784b
Hussein <hussein0116>
2019-02-24 21:26:59 +0200

    ImageLoader init

--- app/src/main/java/com/hussein/imageloaderlibrary/MainActivity.kt
+++ app/src/main/java/com/hussein/imageloaderlibrary/MainActivity.kt
@@ -2,7 +2,6 @@ package com.hussein.imageloaderlibrary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
