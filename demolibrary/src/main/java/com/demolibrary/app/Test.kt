68dfff6534826270063f2a4173e80557ea539aec
Hussein <hussein0116>
2019-02-27 22:51:22 +0200

    ImageLoader init

---
+++ demolibrary/src/main/java/com/demolibrary/app/Test.kt
@@ -0,0 +1,10 @@
+package com.demolibrary.app
+
+import android.content.Context
+import android.widget.Toast
+
+class Test (ctx:Context){
+    init {
+        Toast.makeText(ctx,"Done",Toast.LENGTH_SHORT).show()
+    }
+}
